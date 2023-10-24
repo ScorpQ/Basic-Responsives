c0bc773e11d81f3c7483157793a8d2dc62ce8fbe
ScorpN <sf_commander@outlook.com>
2023-10-25 01:26:40 +0300

    yeto

---
+++ IK-pdedonlyCSS/script.js
@@ -0,0 +1,13 @@
+const primaryNav = document.querySelector('header');
+const navToggle = document.querySelector('.hamburger');
+console.log(primaryNav);
+navToggle.addEventListener("click", () => { 
+    if(primaryNav.getAttribute("data-visible") === "false"){
+        primaryNav.setAttribute("data-visible","true");
+    }
+    else{
+        primaryNav.setAttribute("data-visible","false");
+    }   
+})
+
+
